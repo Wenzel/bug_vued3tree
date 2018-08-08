01a197429fd8ef6a8f99b469dbd189dc4901fcd2
Mathieu Tarral <mathieu.tarral@f-secure.com>
2018-08-08 13:02:08 +0300

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="vued3tree">
+    <tree :data="tree" :node-text="name" layoutType="circular">
+    </tree>
+  </div>
+</template>
+
+<script>
+import {tree} from 'vued3tree'
+
+export default {
+  components: {
+    tree
+  },
+  data() {
+    return {
+      tree: {
+        name: "father",
+        children:[{
+          name: "son1",
+          children:[ {name: "grandson"}, {name: "grandson2"}]
+        },{
+          name: "son2",
+          children:[ {name: "grandson3"}, {name: "grandson4"}]
+        }]
+      }
+    }
+  }
+}
+</script>
+
+<style>
+#vued3tree {
+  height: 600px;
+  width: 100%;
+}
+</style>
